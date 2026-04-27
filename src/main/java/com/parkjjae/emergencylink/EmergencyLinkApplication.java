880f81f36ace41e809226a5b583e78cdd9178124
parkJJae <wgpark0830@naver.com>
2026-04-27 17:20:48 +0900

    chore: 프로젝트 초기 환경 세팅 완료

---
+++ src/main/java/com/parkjjae/emergencylink/EmergencyLinkApplication.java
@@ -0,0 +1,13 @@
+package com.parkjjae.emergencylink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmergencyLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmergencyLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/parkjjae/emergencylink/EmergencyLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.parkjjae.emergencylink;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergencyLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
